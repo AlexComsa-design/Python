20106d19038915eddaff27b05f250f0005665efa
AlexComsa-design <comsaalexandruionel@gmail.com>
2026-06-16 16:04:20 +0100

    test2

--- hello.py
+++ hello.py
@@ -1,2 +1,4 @@
 print("Hello Alexandru")
-print("I am starting my AI journey")
+print("I am starting my AI journey")print("Hello Alexandru")
+print("I am learning Python and Git")
+print("Next step: real programming begins")
